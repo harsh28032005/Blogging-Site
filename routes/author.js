3ac1f16b96a475581efcdfd66f64efa916f53e36
Harsh Gautam <hg324162@gmail.com>
2025-07-25 17:33:34 +0530

    it's ok

--- routes/author.js
+++ routes/author.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { create_author, get_all_author } from "../controllers/author_controller.js";
+import { create_author, get_all_author, login } from "../controllers/author_controller.js";
 
 const router = express.Router();
 
 router.post("/author", create_author);
 router.get("/author", get_all_author);
+router.post("/login", login);
 
 export default router;
--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   create_blog,
   delete_blog,
+  delete_blog_by_query,
   get_blog,
   update_blog,
 } from "../controllers/blog_controller.js";
@@ -12,5 +13,6 @@ router.post("/blogs", create_blog);
 router.get("/blogs", get_blog);
 router.put("/blogs/:blog_id", update_blog);
 router.delete("/blogs/:blog_id", delete_blog);
+router.delete("/blogs", delete_blog_by_query);
 
 export default router;
